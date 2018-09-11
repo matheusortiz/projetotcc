05a3e395e74162f5ed24f5f14cde344c5f422af2
matheusortiz <matheus7@gmail.com>
2018-09-11 15:25:22 -0300

    Projeto criado

---
+++ src/main/java/com/pucminas/tcc/TccinicialApplication.java
@@ -0,0 +1,12 @@
+package com.pucminas.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccinicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccinicialApplication.class, args);
+	}
+}
